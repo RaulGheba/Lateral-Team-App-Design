40379f6bcff4bd85c0c9d070ab433203304e2300
Raul Gheba <raul.gheba15@yahoo.ro>
2023-07-26 13:22:01 +0300

    --all

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// SLIDER CONTROL
+const testimonialSlider = document.querySelector('.slider');
+const testimonialRight = document.querySelector(".slider-right");
+const testimonialLeft = document.querySelector(".slider-left");
+
+
+testimonialRight.addEventListener('click',function(){
+  testimonialSlider.style.marginLeft='-90rem';
+  testimonialSlider.style.transition='all 0.5s';
+  testimonialLeft.style.display = "block";
+  this.style.display='none';
+})
+
+testimonialLeft.addEventListener("click", function () {
+  testimonialSlider.style.marginLeft = "0.5rem";
+  testimonialSlider.style.transition = "all 0.5s";
+  testimonialRight.style.display='block';
+  this.style.display='none';
+});
+
+testimonialRight.addEventListener('mouseenter',function(){
+  this.style.cursor='pointer';
+})
+
+testimonialLeft.addEventListener("mouseenter", function () {
+  this.style.cursor = "pointer";
+});
