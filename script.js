2373fb038ca58247081b3f0572be0e063d297748
Rally <raul.gheba15@yahoo.ro>
2023-08-04 14:39:38 +0300

    --all

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-'use strict';
+"use strict";
 
 // SLIDER CONTROL
-const testimonialSlider = document.querySelector('.slider');
+const testimonialSlider = document.querySelector(".slider");
 const testimonialRight = document.querySelector(".slider-right");
 const testimonialLeft = document.querySelector(".slider-left");
-
-
-testimonialRight.addEventListener('click',function(){
-  testimonialSlider.style.marginLeft='-80rem';
-  testimonialSlider.style.transition='all 0.5s';
-  testimonialLeft.style.display = "block";
-  this.style.display='none';
-})
+const toggleMobile = document.querySelector(".mobile-toggler");
+const mobileNav = document.querySelector(".mobile-nav");
+testimonialRight.addEventListener("click", function () {
+  testimonialSlider.style.marginLeft = "-100rem";
+  testimonialSlider.style.transition = "all 0.5s";
+});
 
 testimonialLeft.addEventListener("click", function () {
   testimonialSlider.style.marginLeft = "0.5rem";
   testimonialSlider.style.transition = "all 0.5s";
-  testimonialRight.style.display='block';
-  this.style.display='none';
 });
 
-testimonialRight.addEventListener('mouseenter',function(){
-  this.style.cursor='pointer';
-})
+testimonialRight.addEventListener("mouseenter", function () {
+  this.style.cursor = "pointer";
+});
 
 testimonialLeft.addEventListener("mouseenter", function () {
   this.style.cursor = "pointer";
 });
+
+// Function to toggle the mobile navigation menu
+function toggleMobileNav() {
+  // Toggle the 'display-mobile-nav' class to show/hide the mobile navigation
+  mobileNav.classList.toggle("display-mobile-nav");
+}
+
+// Add event listener to the mobile-toggler element to toggle the mobile nav
+toggleMobile.addEventListener("click", toggleMobileNav);
